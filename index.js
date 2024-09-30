ac0520d2de2128e929022423b1c8b83bc5c80788
nnoskov <hhb@bk.ru>
2024-09-30 21:47:35 +0300

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Запускаем таймер!');
+
+// alert('5');
+
+// alert('4');
+
+// alert('3');
+
+// alert('2');
+
+// alert('1');
+
+// alert('0');
+
+// console.log('Обратный отсчет закончен!');
+
+for (let index = 5; index > -1; index--){
+    alert(index);
+}
+
+console.log('Обратный отсчет закончен!');
