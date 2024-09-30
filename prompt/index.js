f88864eb64f1da283f327bdd1cba94d46e10ee8a
nnoskov <hhb@bk.ru>
2024-09-30 22:49:02 +0300

    Task #4 js code has been added

--- prompt/index.js
+++ prompt/index.js
@@ -1,6 +1,30 @@
-let ansfer = prompt("Сколько будет 2 + 2?");
-if (ansfer === 4) {
-  alert("Ты прав");
-} else {
-  ("Ты не прав :(");
+function checkNull(ansfer) {
+  if (ansfer === null) alert("Видимо ты отказался отвечать...");
+}
+
+// qw #1
+var ansfer = prompt("Сколько будет 2 + 2?");
+checkNull(ansfer);
+if (ansfer === "4") {
+  alert("Ответ: если ты ответил 4, то ты прав. Похоже ты ответил 4!");
+} else if (ansfer !== null) {
+  alert("Ты не прав :(");
+}
+
+// qw #2
+var ansfer = prompt("Чему равен корень из 9?");
+checkNull(ansfer);
+if (ansfer === "3") {
+  alert("Ответ: если ты ответил 3, то ты прав. Похоже ты ответил 3!");
+} else if (ansfer !== null) {
+  alert("Ты не прав :(");
+}
+
+// qw #3
+var ansfer = prompt("Сколько будет 2 в 5-й степени?");
+checkNull(ansfer);
+if (ansfer === "32") {
+  alert("Ответ: если ты ответил 32, то ты прав. Похоже ты ответил 32!");
+} else if (ansfer !== null) {
+  alert("Ты не прав :(");
 }
