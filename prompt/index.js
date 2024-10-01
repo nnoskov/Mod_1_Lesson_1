daf3f22c4631ae9c8354ee353b36ce9aa6daed72
NIkolay Noskov <hhb@bk.ru>
2024-10-01 19:04:31 +0300

    Update index.js
    Redeclaration of 'answer' has been deleted.

--- prompt/index.js
+++ prompt/index.js
@@ -1,30 +1,30 @@
-function checkNull(ansfer) {
-  if (ansfer === null) alert("Видимо ты отказался отвечать...");
+function checkNull(answer) {
+  if (answer === null) alert("Видимо ты отказался отвечать...");
 }
 
 // qw #1
-var ansfer = prompt("Сколько будет 2 + 2?");
-checkNull(ansfer);
-if (ansfer === "4") {
+var answer = prompt("Сколько будет 2 + 2?");
+checkNull(answer);
+if (answer === "4") {
   alert("Ответ: если ты ответил 4, то ты прав. Похоже ты ответил 4!");
-} else if (ansfer !== null) {
+} else if (answer !== null) {
   alert("Ты не прав :(");
 }
 
 // qw #2
-var ansfer = prompt("Чему равен корень из 9?");
-checkNull(ansfer);
-if (ansfer === "3") {
+answer = prompt("Чему равен корень из 9?");
+checkNull(answer);
+if (answer === "3") {
   alert("Ответ: если ты ответил 3, то ты прав. Похоже ты ответил 3!");
-} else if (ansfer !== null) {
+} else if (answer !== null) {
   alert("Ты не прав :(");
 }
 
 // qw #3
-var ansfer = prompt("Сколько будет 2 в 5-й степени?");
-checkNull(ansfer);
-if (ansfer === "32") {
+answer = prompt("Сколько будет 2 в 5-й степени?");
+checkNull(answer);
+if (answer === "32") {
   alert("Ответ: если ты ответил 32, то ты прав. Похоже ты ответил 32!");
-} else if (ansfer !== null) {
+} else if (answer !== null) {
   alert("Ты не прав :(");
 }
